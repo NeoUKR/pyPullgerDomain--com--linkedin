895c6ee46d019deaaa83b92297d4e8a677c927cf
NeoUKR <neokkv@gmail.com>
2022-07-27 14:45:01 +0300

    Update version discribtion

--- version.py
+++ version.py
@@ -9,4 +9,22 @@ __version__ = VERSION_INFO
 #   PullgerDomain.com.linkedin.port
 #   PullgerFootPrint.com.linkedin
 
-# Functions:
+
+
+# Functionality
+# Autorization
+# - standard autorization
+# - pass require phone insertion (PDCL#0010)
+# - mechanisme for pass check
+
+
+
+# LoadPeoples
+#   Exceptions:
+#       unexistingPages (PDCL#0010) (example people: '48c52169-0422-11ed-bc1f-b5c012259935' ,
+#       stop loading people on error
+#       correct check on rendering  (add rull)
+#       fix (add rull) non language links (rewrite some block of code)
+#   Load company Experiance list:
+#       load full experiance
+#       load multi job position in one compani
